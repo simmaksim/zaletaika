1be02df08d423b61d9cb2a85900a4cbc59fd37a1
Kryshtapovich Mikhail <61320447+Kryshtapovich@users.noreply.github.com>
2022-11-17 19:19:45 +0300

    ass

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -3,28 +3,25 @@ import Button from "../UI/Button/Button"
 import { useRef } from "react"
 
 export function LoginPage(){
-    let login = useRef()
+    const login = useRef()
     
-    // const hello = () =>{
-    //     console.log("HEUEUE")
-    //     console.log(login.current.value)
-    // }
-    function hello(){
+    const hello = () => {
         console.log("HEUEUE")
         console.log(login.current.value)
+        if(!login.current.value) {
+            alert('pidor');
+        }
     }
+
     return (
-        <>
             <div className={classes.loginForm}>
                 <div className={classes.loginContainer}>
                     <h2 className={classes.loginText}>Login</h2>
                     <input className={classes.loginInput} placeholder="Enter your login" ref={login} />
                     <h2 className={classes.loginText}>Password</h2>
                     <input className={classes.loginInput} placeholder="Enter your password"/>
-                    {/* <Button onclick={hello}>Hueta</Button> */}
-                    <button onClick={hello()}>hueta</button>
+                    <button onClick={hello}>hueta</button>
                 </div>
             </div>
-        </>
     )
 }
